9715617591809f37b0db036e9e54cc590c058142
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-07-13 00:14:39 +0530

    Delete arrays in JavaScript.js

--- arrays in JavaScript.js
+++
@@ -1,9 +0,0 @@
-const languages = ['C', 'C++', 'JavaScript', 'Java']
-
-console.log(languages);
-
-// to find array length
-console.log(languages.length);
-
-// to find value of spacific index element 
-console.log(languages [1 /*index numer*/]);
