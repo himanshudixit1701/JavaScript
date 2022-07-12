c3ac61900f6dce665bdde19a3b5936205f3025f5
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-07-12 23:59:52 +0530

    arrays in JavaScript

---
+++ arrays in JavaScript.js
@@ -0,0 +1,9 @@
+const languages = ['C', 'C++', 'JavaScript', 'Java']
+
+console.log(languages);
+
+// to find array length
+console.log(languages.length);
+
+// to find value of spacific index element 
+console.log(languages [1 /*index numer*/]);
