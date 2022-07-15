1c376147614fc07019f33f5607739d197426233f
Himanshu Dixit <101091797+himanshudixit1701@users.noreply.github.com>
2022-07-15 23:10:22 +0530

    filter in JavaScript

---
+++ filter in JavaScript.js
@@ -0,0 +1,27 @@
+const students = [
+    {
+        name: 'stdent 1',
+        marks: 45
+    },
+    {
+        name: 'student 2',
+        marks: 60
+    },
+    {
+        name: 'student 3',
+        marks: 35
+    }
+];
+
+    // students.filter((student) => {
+    //      console.log(student);
+    // });
+
+    const failed = students.filter((student) => {
+        if (student.marks < 45){
+            return true;
+        } else{
+            return false;
+        }
+    });
+    console.log(failed);
